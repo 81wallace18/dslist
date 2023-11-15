e6f573c5787198948f1c6cd513b4ce9d1af0fd6c
Wallace Patrick <patrickwallace621@gmail.com>
2023-11-15 17:09:14 -0300

    Game seed

--- src/main/java/com/wallace/dslist/entities/Game.java
+++ src/main/java/com/wallace/dslist/entities/Game.java
@@ -12,38 +12,42 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_game")
-public class Game implements Serializable{
-	
+public class Game implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String tittle;
-	
+	private String title;
+
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
 	private String platforms;
 	private Double score;
 	private String imgUrl;
-	private String shortDescription;
-	private String longDescripton;
 	
+	@Column(columnDefinition = "TEXT")
+	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
+	private String longDescription;
+
 	public Game() {
 	}
 
-	public Game(Long id, String tittle, Integer year, String genre, String platforms, Double score, String imgUrl,
-			String shortDescription, String longDescripton) {
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+			String shortDescription, String longDescription) {
 		this.id = id;
-		this.tittle = tittle;
+		this.title = title;
 		this.year = year;
 		this.genre = genre;
 		this.platforms = platforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
-		this.longDescripton = longDescripton;
+		this.longDescription = longDescription;
 	}
 
 	public Long getId() {
@@ -54,12 +58,12 @@ public class Game implements Serializable{
 		this.id = id;
 	}
 
-	public String getTittle() {
-		return tittle;
+	public String getTitle() {
+		return title;
 	}
 
-	public void setTittle(String tittle) {
-		this.tittle = tittle;
+	public void setTittle(String title) {
+		this.title = title;
 	}
 
 	public Integer getYear() {
@@ -110,12 +114,12 @@ public class Game implements Serializable{
 		this.shortDescription = shortDescription;
 	}
 
-	public String getLongDescripton() {
-		return longDescripton;
+	public String getLongDescription() {
+		return longDescription;
 	}
 
-	public void setLongDescripton(String longDescripton) {
-		this.longDescripton = longDescripton;
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
 	}
 
 	@Override
